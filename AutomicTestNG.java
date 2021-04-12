01ee2757ed8e9d9ec8cc7a06d3a53614b6792768
My-code16 <82297398+My-code16@users.noreply.github.com>
2021-04-12 11:31:59 +1000

    Update AutomicTestNG.java

--- AutomicTestNG.java
+++ AutomicTestNG.java
@@ -10,7 +10,7 @@ public class AutomicTestNG {
 	public static void main(String[] args) {
 		TestNG testngmain = new TestNG();
 		List<String> suites = new ArrayList<String>();
-		suites.add("C:\\Users\\MadhusudhanPillai\\eclipse-workspace\\automicgrp\\testngmain.xml");
+		suites.add("/automicgrp/testngmain.xml");
 		testngmain.setTestSuites(suites);
 		testngmain.setTestClasses(new Class[] { Abnautomation_run.class });
 		testngmain.run();
